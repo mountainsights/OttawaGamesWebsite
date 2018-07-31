2cb56987546e1f51f9575b9fa2ed4c6021fcdebd
Fuad-PC\Fuad <mfuad.chow@gmail.com>
2018-07-30 21:44:54 -0400

    Grid layout
    -Changed to grid layout
    -Added smalls bits of js for testing
    -tested with background images etc.

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("loggedIn").style.display="none";
+
+function myFunction() {
+    document.getElementById("loganpaul").style.display="none";
+    newFuntion();
+}
+
+function newFuntion () {
+		document.getElementById("loggedIn").style.display="block";
+	    document.getElementById("loggedIn").innerHTML = "Welcome! you are logged in as: (your Mommy)" 
+
+}
